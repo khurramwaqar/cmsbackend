8ea746fd28340dfb1a2dc580e33734abf9572da4
khurram waqar <khurrambaigw@gmail.com>
2024-12-12 18:35:45 +0500

    fixed dynamic fields

--- src/pages/series/Create.js
+++ src/pages/series/Create.js
@@ -497,7 +497,7 @@ const SeriesCreate = () => {
                                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                     placeholder="Series Type: show|live|singleVideo|webview"
                                     required /> */}
-                                <select required {...register("seriesTypeR")} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                                <select {...register("seriesTypeR")} onChange={(e) => eventChangeFunc(e.target.value)} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                                     <option value={'show'}> Show </option>
                                     <option value={'live-event'}> Live Event </option>
                                     <option value={'live'}> Live </option>
